5734c5d6a8e8a414bf65fdaf520820c858085a03
Tyler Marefke <57193675+Kirbitz@users.noreply.github.com>
2021-05-06 13:30:48 -0500

    Add files via upload
    Uploading Project 3 from DS

---
+++ Project3/VarArray_Project.cpp
@@ -0,0 +1,32 @@
+#include "varList.h";
+
+int main() {
+	//Declares variables required for the main method
+	VarList<int> myList;
+	char selectionAns;
+	int numAns;
+	
+	do {
+
+		cout << "Enter operation [A/R/Q] :"; cin >> selectionAns;
+
+		switch (toupper(selectionAns))
+		{
+		case 'A':
+			cout << "Enter a value\t\t:"; cin >> numAns;
+			myList.addValue(numAns);
+			break;
+		case 'R':
+			cout << "Enter a value\t\t:"; cin >> numAns;
+			myList.removeValue(numAns);
+		case 'Q':
+			break;
+		default:
+			cout << "Your selection was not A, R, or Q!\nPlease try again!\n";
+			break;
+		}
+
+	} while (toupper(selectionAns) != 'Q');
+
+	return 0;
+}
